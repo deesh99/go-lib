95b171b066a90ee0c164c52e0e7db0f4b9bcc101
deesh99 <deesh99@gmail.com>
2023-03-09 03:40:45 +0530

    Update demo.go

--- demo.go
+++ demo.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Println(Add(2, 3))
-	//fmt.Println(Subtract(2, 3))
+	fmt.Println(Add(2, 3))
+	fmt.Println(Subtract(2, 3))
 	fmt.Println("Hello!")
 }
